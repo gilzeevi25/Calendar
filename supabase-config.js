d2d1f62d23150a6eade988a95f77e159a225cd3f
Gil Zeevi <gilsmol91@gmail.com>
2026-02-23 22:50:40 +0200

    Remove comment from supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://kcujtvxekwsrycjzeiuy.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_JUe7k3OEU0yTigfkYUk-SQ_LeHFwKHB';
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
